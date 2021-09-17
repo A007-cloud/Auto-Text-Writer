7d0aa859f52836e73e07eb46abd03361217683af
A007-cloud <dasaditya788@gmail.com>
2021-09-17 13:36:38 +0530

    init

---
+++ main.js
@@ -0,0 +1,15 @@
+const text = "Good to see you here!!";
+
+let index = 0;
+
+function writeText() {
+    document.body.innerHTML = text.slice(0, index);
+    
+    index++;
+
+    if(index > text.length) {
+        index = 0;
+    }
+};
+
+setInterval(writeText, 100);
